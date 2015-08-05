e8a2cb9dd211476b4841a1434fd311c902a65b95
Richard-Murray <arraygear2@gmail.com>
2015-08-05 11:13:21 +1000

    Updated moving platform
    Minor update to test version control

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -29,6 +29,7 @@ public class PlatformController : RaycastController {
 		float directionX = Mathf.Sign (velocity.x);
 		float directionY = Mathf.Sign (velocity.y);
 
+		//Vertical movement
 		if (velocity.y != 0) {
 			float rayLength = Mathf.Abs (velocity.y) + skinWidth;
 			
@@ -52,8 +53,50 @@ public class PlatformController : RaycastController {
 			}
 		}
 
+		//Horizontal movement
 		if (velocity.x != 0) {
+			float rayLength = Mathf.Abs (velocity.x) + skinWidth;
+			
+			for (int i = 0; i < horizontalRayCount; i++) {
+				Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
+				rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+				RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
 
+				if(hit){
+					if(!movedPassengers.Contains (hit.transform)){
+						movedPassengers.Add (hit.transform);
+						
+						float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+						float pushY = 0;
+						
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}
+				}
+			}
+		}
+
+		//Passengering
+
+		if (directionY == -1 || velocity.y == 0 && velocity.x != 0) {
+			float rayLength = 2 * skinWidth;
+			
+			for (int i = 0; i < verticalRayCount; i++) {
+				Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
+				RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.up, rayLength, passengerMask);
+				
+
+				
+				if(hit){
+					if(!movedPassengers.Contains (hit.transform)){
+						movedPassengers.Add (hit.transform);
+						
+						float pushX = velocity.x;
+						float pushY = velocity.y;
+						
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}
+				}
+			}
 
 
 		}
