56362dc6aee23b09ac17d3ca16d1d125ac4836c6
None <arraygear2@gmail.com>
2015-08-14 15:08:17 +1000

    Almost finished walljumping
    Still cannot walljump while travelling 'up' the wall, this will be fixed
    later.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,9 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	[Header("Movement")]
-	public float jumpHeight = 4;
+	public bool canJump;
+	public float maxJumpHeight = 4;
+	public float minJumpHeight = 1;
 	public float timeToJumpApex = 0.4f;
 	float accelerationTimeAirborne = 0.2f;
 	float accelerationTimeGrounded = 0.1f;
@@ -25,7 +27,8 @@ public class Player : MonoBehaviour {
 	//float gravity = -20;
 	//float jumpVelocity = 8;
 	float gravity;
-	float jumpVelocity;
+	float maxJumpVelocity;
+	float minJumpVelocity;
 	Vector3 velocity;
 	float velocityXSmoothing;
 
@@ -35,9 +38,9 @@ public class Player : MonoBehaviour {
 	void Start () {
 		controller = GetComponent<Controller2D>();
 
-		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
-		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
-		print ("Gravity: " + gravity + "Jump Velocity: " + jumpVelocity);
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		maxJumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+		print ("Gravity: " + gravity + "Jump Velocity: " + maxJumpVelocity);
 	}
 	
 	// Update is called once per frame
@@ -45,7 +48,26 @@ public class Player : MonoBehaviour {
 		//Get input vector
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		int wallDirectionX = (controller.collisions.left) ? -1 : 1;
+		float targetVelocityX = 0;
 
+		
+		//velocity.x = input.x * moveSpeed;
+
+		//Calculate x velocity
+		//if (!(input.x < 0 && controller.collisions.left) && !(input.x > 0 && controller.collisions.right)) {
+		targetVelocityX += input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+		//}
+
+		//Zero velocities if there is a collision on that axis to prevent velocities increasing during collision
+		//TODO: THIS MAY KILL WALLJUMPING BUT IS IMPORTANT
+		/*if ((controller.collisions.left && velocity.x < 0) || (controller.collisions.right && velocity.x > 0)) 
+			velocity.x = 0;*/
+			//velocity.x = velocity.x + controller.collisions.platformVelocity.x;
+
+		//maybe do after walljumping calculations
+
+		//Wallsliding
 		bool wallSliding = false;
 		if (canWallJump == true) {
 			if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
@@ -56,11 +78,18 @@ public class Player : MonoBehaviour {
 				}
 
 				if (timeToWallUnstick > 0) {
+
+					velocityXSmoothing = 0;
+					velocity.x = 0;
+
 					if (input.x != wallDirectionX && input.x != 0) {
 						timeToWallUnstick -= Time.deltaTime;
 					} else {
-
+						timeToWallUnstick = wallStickTime;
 					}
+				}
+				else{
+					timeToWallUnstick = wallStickTime;
 				}
 			}
 		}
@@ -70,14 +99,12 @@ public class Player : MonoBehaviour {
 			velocity.y = 0;
 		}
 
-		float targetVelocityX = 0;
-
 		timeSinceGrounded += Time.deltaTime;
 		if (controller.collisions.below == true)
 			timeSinceGrounded = 0;
 
-		//Take the platform's velocity into account when jumping
-		if(Input.GetKeyDown (KeyCode.Space)/* && (controller.collisions.below || timeSinceGrounded < 0.1f)*/) { //want to add buffered jumps
+		//Takes the platform's velocity into account when jumping
+		if(Input.GetKeyDown (KeyCode.Space) && canJump/* && (controller.collisions.below || timeSinceGrounded < 0.1f)*/) { //want to add buffered jumps
 			if(wallSliding && canWallJump == true)	{
 				if(wallDirectionX == input.x){
 					velocity.x = -wallDirectionX * wallJumpClimb.x;
@@ -94,27 +121,15 @@ public class Player : MonoBehaviour {
 			}
 			else if(controller.collisions.below || timeSinceGrounded < 0.1f)
 			{
-				velocity.y = jumpVelocity + controller.collisions.platformVelocity.y;
+				velocity.y = maxJumpVelocity + controller.collisions.platformVelocity.y;
 				velocity.x = velocity.x + controller.collisions.platformVelocity.x;
 				targetVelocityX = controller.collisions.platformVelocity.x;
 			}
 		}
 
-		//velocity.x = input.x * moveSpeed;
-
-		//Calculate x velocity
-		if (!(input.x < 0 && controller.collisions.left) && !(input.x > 0 && controller.collisions.right)) {
-			targetVelocityX += input.x * moveSpeed;
-			velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
-		}
-
-		//Zero velocities if there is a collision on that axis
-		//TODO: THIS MAY KILL WALLJUMPING
-		/*if ((controller.collisions.left && velocity.x < 0) || (controller.collisions.right && velocity.x > 0)) 
+		if (((velocity.x < 0 && controller.collisions.left) || (velocity.x > 0 && controller.collisions.right)) && !wallSliding) {
 			velocity.x = 0;
-		if (controller.collisions.above)
-			velocity.y = 0;*/
-			//velocity.x = velocity.x + controller.collisions.platformVelocity.x;
+		}
 
 		//Send move command through to the controller2D
 		velocity.y += gravity * Time.deltaTime;
